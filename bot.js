f82804f1651fc76d559ce16fff90e716aef000bc
Fitsum <fitsesile@gmail.com>
2023-09-05 22:40:12 +0300

    (feat): Added command handling

--- bot.js
+++ bot.js
@@ -35,3 +35,36 @@ bot.on('webhook_error', (error) => {
     console.error('Webhook error:', error);
 });
 
+module.exports = async (req, res) => {
+    
+    res.setHeader('Content-Type', 'application/json');
+
+    try {
+      const data = req.body;
+      const updateType = data.message.text; 
+      const chatId = data.message.chat.id;
+      const text = data.message.text;
+      const command = updateType.split(' ')[0];
+      
+      const tasksCollection = await initializeMongoDB();
+  
+    switch (command) 
+        {
+        case  '/start':
+            bot.sendMessage(chatId, 'Welcome to your task management bot! What would you like to do?');
+            break;
+        
+        default:
+            bot.sendMessage(data.message.chat.id, 'I do not understand that command. Please use /start, /create, /list, or /done.');
+        }
+    
+        res.status(200).send(JSON.stringify({ success: true }));
+    } 
+    
+    catch (error) 
+    {
+      console.error('Error handling request:', error);
+      res.status(500).send(JSON.stringify({ success: false, error: 'Internal Server Error' }));
+    }
+  };
+
